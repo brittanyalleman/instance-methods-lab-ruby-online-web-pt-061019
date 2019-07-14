887827fcb1e0a5f6bdb990921096cf05cc9eb582
Brittany Alleman <brittany.alleman1@gmail.com>
2019-07-14 19:17:57 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog
+
+#def initialize (name)
+#end
+
+def name
+  return name
+end
+
+def name= (name)
+@name = name
+end
+
+def bark
+puts "Woof!"
+end
+
+def sit
+  puts "plop"
+end
+
+fido = Dog.new
+fido.name ="fido"
+end
